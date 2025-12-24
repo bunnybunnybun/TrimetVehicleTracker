73a0bbfa8e103dbfa166d3dbc916c8009a5e7022
Carlisle <carlislepoe@gmail.com>
2025-12-23 22:37:00 -0800

    Initial commit! Createthe start of my app, currently it fetches the data about transportation routes from Trimet and lets you select which route and which stop along that route you want.

---
+++ main.py
@@ -0,0 +1,77 @@
+import requests
+import json
+import os
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gdk
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+appID = "9BB16D426AE6D7BB1EDAED215"
+routesUrl = f"https://developer.trimet.org/ws/V1/routeConfig?appID={appID}&json=true&dir=true&stops=1"
+
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Trimet Bus Tracker")
+        self.set_border_width(20)
+
+        self.css_provider = Gtk.CssProvider()
+        self.css_provider.load_from_path(f"{script_dir}/style.css")
+        screen = Gdk.Screen.get_default()
+        style_context = Gtk.StyleContext()
+        style_context.add_provider_for_screen(
+            screen,
+            self.css_provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+
+        self.routes_data = []
+
+        def getRoutes():
+            self.routesResponse = requests.get(routesUrl).json()
+            self.routes_data = self.routesResponse["resultSet"]["route"]
+
+            for route in self.routes_data:
+                self.route_dropdown.append_text(route["desc"])
+
+
+        self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        self.routesLabel = Gtk.Label(label="Select route:")
+
+        self.route_dropdown = Gtk.ComboBoxText()
+        self.route_dropdown.set_entry_text_column(0)
+        self.route_dropdown.connect("changed", self.on_route_selected)
+
+        self.stop_dropdown = Gtk.ComboBoxText()
+        self.stop_dropdown.set_entry_text_column(0)
+        
+        self.main_box.add(self.routesLabel)
+        self.main_box.add(self.route_dropdown)
+        self.main_box.add(self.stop_dropdown)
+        self.add(self.main_box)
+
+
+        getRoutes()
+    
+    def on_route_selected(self, combo):
+        index = combo.get_active()
+        if index == -1:
+            return
+        
+        selected_route = self.routes_data[index]
+        route_number = selected_route["route"]
+
+        print(f"selected route: {route_number}")
+        
+        for direction in selected_route["dir"]:
+            direction_name = direction["desc"]
+
+            for stop in direction["stop"]:
+                stop_text = f"{direction_name} - {stop['desc']}"
+                self.stop_dropdown.append_text(stop_text)
+
+win = MainWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
